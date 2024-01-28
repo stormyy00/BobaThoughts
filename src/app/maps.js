0a7a6a1e53d55bfaec072a927956ee7d017c623b
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-01-27 22:49:28 -0800

    adding places

--- src/app/maps.js
+++ src/app/maps.js
@@ -1,6 +1,11 @@
 "use client";
-import React, { useState } from "react";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import React, { useState, useEffect } from "react";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  Marker,
+  Autocomplete,
+} from "@react-google-maps/api";
 
 const Maps = () => {
   const containerStyle = {
@@ -9,13 +14,14 @@ const Maps = () => {
   };
 
   const center = {
-    lat: -3.745,
-    lng: -38.523,
+    lat: 34.0549,
+    lng: -118.2426,
   };
 
   const { isLoaded, loadError } = useJsApiLoader({
     id: "google-map-script",
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY,
+    libraries: ["places"],
   });
 
   const [map, setMap] = useState(null);
@@ -30,22 +36,105 @@ const Maps = () => {
     setMap(null);
   }, []);
 
+  const initAutocomplete = (google, map) => {
+    const input = document.getElementById("pac-input");
+    const searchBox = new google.maps.places.SearchBox(input);
+
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    map.addListener("bounds_changed", () => {
+      searchBox.setBounds(map.getBounds());
+    });
+
+    let markers = [];
+
+    searchBox.addListener("places_changed", () => {
+      const places = searchBox.getPlaces();
+
+      if (places.length === 0) {
+        return;
+      }
+
+      markers.forEach((marker) => {
+        marker.setMap(null);
+      });
+      markers = [];
+
+      const bounds = new google.maps.LatLngBounds();
+
+      places.forEach((place) => {
+        if (!place.geometry || !place.geometry.location) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+
+        const icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25),
+        };
+
+        markers.push(
+          new google.maps.Marker({
+            map,
+            icon,
+            title: place.name,
+            position: place.geometry.location,
+          })
+        );
+        if (place.geometry.viewport) {
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
+  };
+
+  useEffect(() => {
+    if (isLoaded) {
+      const google = window.google;
+
+      if (google && map) {
+        console.log("Google and map are available:", google, map);
+        initAutocomplete(google, map);
+      } else {
+        console.log("Google or map is not available:", google, map);
+      }
+    }
+  }, [isLoaded, map]);
+
   if (loadError) {
     return <div>Error loading Google Maps API script</div>;
   }
 
   return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={10}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-    >
-      {/* Optional: Add additional map components or features here */}
-    </GoogleMap>
+    <>
+      <Autocomplete>
+        <input
+          id="pac-input"
+          type="text"
+          placeholder="Search places..."
+          className="m-2 w-1/2 p-3"
+        />
+      </Autocomplete>
+
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        {/* Optional: Add additional map components or features here */}
+      </GoogleMap>
+    </>
   ) : (
     <div>Loading...</div>
   );
 };
+
 export default Maps;
