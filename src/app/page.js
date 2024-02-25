6e857e17fae4235005f4d00d009010a17f61f0e3
Victor Wongsatjachock <victorwongsatjachock@gmail.com>
2024-02-24 16:00:38 -0800

    login, signup working

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import Maps from "../components/maps";
 import Logo from "../../public/logo.svg";
-import Signin from "../components/Signin";
+import Header from "../components/Header";
+
 
 const page = () => {
   return (
     <div className="flex flex-col">
-      <Signin />
-
+      <Header />
+      <Maps />
     </div>
   );
 };
