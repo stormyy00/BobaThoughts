5ac0237b1b26e65488dec7dbd95e7cf4d3d03397
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-02-28 00:29:05 -0800

    working api key

--- src/components/Yelp.jsx
+++ src/components/Yelp.jsx
@@ -1,29 +1,47 @@
 "use client";
 import { useState, useEffect } from "react";
-import axios from "axios";
 
 const YelpSearch = () => {
   const [businesses, setBusinesses] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [location, setLocation] = useState("");
 
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+  const handleChange1 = (event) => {
+    setLocation(event.target.value);
+  };
   const handleSearch = async () => {
     try {
       const response = await fetch(
-        `/api/fetch?term=${searchTerm}&location=${location}`
+        `https://proxy.cors.sh/https://api.yelp.com/v3/businesses/search?term=${searchTerm}&location=${location}`,
+        {
+          // mode: "no-cors",
+          headers: {
+            Authorization:
+              "Bearer oMxyNN8K_x1GTfigxjR0npSd3la7VETdEJYq6MqczLVfIx3fYPjkIJLp6qqaXjPjSrcA_R5tsAR5mTpiHX8DIgUbr7fetqGuNOD7F5RqVVwaUC3XFfmVEvUg2s3eZXYx",
+            accept: "application/json",
+            "x-requested-with": "xmlhttprequest",
+            "Access-Control-Allow-Origin": "*",
+            Origin: "localhost:3000",
+            "x-cors-api-key": "temp_4407c675df9080e95c5e6bfd9e7089d8",
+          },
+        }
       );
       if (!response.ok) {
         throw new Error("Failed to fetch data");
       }
       const data = await response.json();
+      console.log(data);
       setBusinesses(data);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
-  useEffect(() => {
-    handleSearch();
-  }, [searchTerm, location]);
+  //   useEffect(() => {
+  //     handleSearch();
+  //   }, [searchTerm, location]);
 
   return (
     <div className="flex flex-col justify center w-full text-black">
@@ -32,30 +50,34 @@ const YelpSearch = () => {
           type="text"
           placeholder="Enter business name"
           value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
+          onChange={handleChange}
         />
         <input
           type="text"
           placeholder="Enter location"
           value={location}
-          onChange={(e) => setLocation(e.target.value)}
+          onChange={handleChange1}
         />
       </div>
       <button className="bg-red-300" onClick={handleSearch}>
         Search
       </button>
 
-      <div className="bg-green-300 ">
-        {businesses.map((business, index) => (
-          <div key={index} className="flex flex-col">
-            <p>{business.name}</p>
-            <p>Rating: {business.rating}</p>
-            <p>
-              Location: {business.location.address1}, {business.location.city}
-            </p>
-          </div>
-        ))}
-      </div>
+      {businesses.businesses && businesses.businesses.length > 0 ? (
+        <div className="bg-green-300">
+          {businesses.businesses.map((business, index) => (
+            <div key={index} className="flex flex-col">
+              <p>{business.name}</p>
+              <p>Rating: {business.rating}</p>
+              <p>
+                Location: {business.location.address1}, {business.location.city}
+              </p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>No businesses found</p>
+      )}
     </div>
   );
 };
