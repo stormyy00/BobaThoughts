cdf73b72f0e13f37d6cef2eb5986680470c48fe7
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-03-01 16:06:50 -0800

    added nav data

---
+++ src/data/nav.js
@@ -0,0 +1,22 @@
+export const items = [
+  {
+    name: "Home",
+    link: "/",
+    icon: "",
+  },
+  {
+    name: "yelp",
+    link: "/yelp",
+    icon: "",
+  },
+  {
+    name: "about",
+    link: "/about",
+    icon: "",
+  },
+  {
+    name: "gallery",
+    link: "/gallery",
+    icon: "",
+  },
+];
