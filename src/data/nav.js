7ad81eab452eb12e001a4a07293148a33ef3369d
Paul Yokota <phyokota@gmail.com>
2024-03-03 23:59:43 -0800

    updated components

--- src/data/nav.js
+++ src/data/nav.js
@@ -1,9 +1,27 @@
+// export const items = [
+//   {
+//     name: "Home",
+//     link: "/",
+//     icon: "",
+//   },
+//   {
+//     name: "yelp",
+//     link: "/yelp",
+//     icon: "",
+//   },
+//   {
+//     name: "about",
+//     link: "/about",
+//     icon: "",
+//   },
+//   {
+//     name: "gallery",
+//     link: "/gallery",
+//     icon: "",
+//   },
+// ];
+
 export const items = [
-  {
-    name: "Home",
-    link: "/",
-    icon: "",
-  },
   {
     name: "yelp",
     link: "/yelp",
