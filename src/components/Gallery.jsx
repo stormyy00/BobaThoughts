f2974b39118a79cb7d30c173de74362b164e4d2d
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-03-11 22:23:12 -0700

    formating fix

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,44 +1,49 @@
-"use client"
+"use client";
 import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import getImages from "@/firebase/firestore/getImages";
 
-
 const Gallery = () => {
+  const [images, setImages] = useState([]);
 
-const [images, setImages] = useState([]);
-
-useEffect(() => {
+  useEffect(() => {
     const fetchImages = async () => {
-    const imagesData = await getImages();
-    setImages(imagesData);
+      const imagesData = await getImages();
+      setImages(imagesData);
     };
 
     fetchImages();
-}, []);
+  }, []);
 
-return (
+  return (
     <div>
-        <Header/>
-        <div className="max-w-6xl mx-auto px-4">
+      <Header />
+      <div className="max-w-6xl mx-auto px-4">
         <h1 className="text-3xl font-semibold mb-4 mt-6">image gallery</h1>
         <div className="flex flex-wrap mx-4 mt-6">
-            {images.map((image, index) => (
-            <div key={index} className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mb-8">
-                <a href={image.link} target="_blank" rel="noopener noreferrer">
+          {images.map((image, index) => (
+            <div
+              key={index}
+              className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mb-8"
+            >
+              <a href={image.link} target="_blank" rel="noopener noreferrer">
                 <div className="bg-white rounded-lg shadow-md overflow-hidden">
-                    <img src={image.url} alt={image.name} className="w-full h-auto" />
-                    <div className="p-4">
+                  <img
+                    src={image.url}
+                    alt={image.name}
+                    className="w-full h-auto"
+                  />
+                  <div className="p-4">
                     <p className="text-lg font-semibold mb-2">{image.name}</p>
-                    </div>
+                  </div>
                 </div>
-                </a>
+              </a>
             </div>
-            ))}
+          ))}
         </div>
-        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Gallery;
